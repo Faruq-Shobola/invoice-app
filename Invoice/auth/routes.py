4df77311ea81b771b70294b0de2385088bfbe02f
shobola faruq <shobolafaruq@gmail.com>
2023-08-14 16:31:48 +0100

    Create user registration form create form and display it on sign-up page

--- Invoice/auth/routes.py
+++ Invoice/auth/routes.py
@@ -1,9 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from .forms import RegistrartionForm
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def home():
   return "Hello World"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+  form = RegistrartionForm()
+  
+  return render_template('sign_up.html', title='Sign up', form=form)
 
 
