f427d94a2a1a9bb1f2fff3330f0683ca6e2277e4
shobola faruq <shobolafaruq@gmail.com>
2023-08-16 01:00:31 +0100

    Restrict logged in user from page Restrict logged in user from signup and signin page and some fixes

--- Invoice/auth/forms.py
+++ Invoice/auth/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 
-class RegistrartionForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
--- Invoice/auth/routes.py
+++ Invoice/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 from invoice import db, bcrypt
-from invoice.auth.forms import RegistrartionForm, LoginForm
+from invoice.auth.forms import RegistrationForm, LoginForm
 from invoice.auth.models import User
 
 auth = Blueprint('auth', __name__)
@@ -14,7 +14,10 @@ def home():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    form = RegistrartionForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.home'))
+    
+    form = RegistrationForm()
 
     if form.validate_on_submit():
         email = form.email.data
@@ -31,6 +34,9 @@ def sign_up():
 
 @auth.route('/signin', methods=['Get', 'POST'])
 def sign_in():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.home'))
+    
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -38,9 +44,9 @@ def sign_in():
         if user and password:
             login_user(user, remember=form.remember.data)
             next = request.args.get('next')
+            flash(f'{user.username} your are logged in', 'info')
             return redirect(next) if next else redirect(url_for('auth.home'))
         
-            flash(f'{user.username} your are logged in', 'info')
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('signin.html', title="Sign in", form=form)
