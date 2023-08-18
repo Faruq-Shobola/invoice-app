5406c8fbd80f77c1f5ffda17ffa664349d053b4c
shobola faruq <shobolafaruq@gmail.com>
2023-08-18 02:41:03 +0100

    Add client details to db

--- Invoice/client/routes.py
+++ Invoice/client/routes.py
@@ -1,18 +1,37 @@
 from flask import redirect, render_template, url_for, flash, Blueprint
+from flask_login import login_required, current_user
+from invoice import db
 from invoice.client.forms import ClientForm
+from invoice.models import Client
 
 client = Blueprint('client', __name__)
 
 
 @client.route('/clients')
+@login_required
 def clients():
     
     return render_template('client/clients.html', title='Clients')
 
 
 @client.route('/add-client', methods=['GET', 'POST'])
+@login_required
 def add_client():
     form = ClientForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        client_type = form.client_type.data
+        email = form.email.data
+        phone = form.number.data
+        website = form.website.data
+        logo = form.logo.data
+        address = form.address.data
+        client = Client(name=name, client_type=client_type, email=email, phone=phone, website=website, logo=logo, address=address, customer=current_user)
+        db.session.add(client)
+        db.session.commit()
+        flash('New client added','info')
+        return redirect(url_for('client.clients'))
+        
     
     return render_template('/client/addclient.html', title='Add Client', form=form)
 
