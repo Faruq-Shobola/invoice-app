b85c5f83a3374cc12fd99f10f4b1ca2a5d963145
shobola faruq <shobolafaruq@gmail.com>
2023-08-25 10:45:57 +0100

    Display new invoice form

--- Invoice/invoice/forms.py
+++ Invoice/invoice/forms.py
@@ -5,15 +5,15 @@ from wtforms.validators import DataRequired
 
 class InvoiceItemForm(FlaskForm):
     item_name = StringField('Item Name', validators=[DataRequired()])
-    item_details = StringField('Item Details')
-    unit_price = DecimalField('Price', validators=[DataRequired()])
-    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    item_details = TextAreaField('Item Details')
+    rate = DecimalField('rate', validators=[DataRequired()], places=2)
+    quantity = IntegerField('Quantity' ,validators=[DataRequired()])
 
 
 class InvoiceForm(FlaskForm):
     client = SelectField('Client', validators=[DataRequired()], coerce=int)
-    invoice_date = DateField('Date Created', DataRequired())
+    invoice_date = DateField('Date Created', validators=[DataRequired()])
     due_date = DateField('Due Date', validators=[DataRequired()])
-    items = FieldList(FormField(InvoiceItemForm), validators=[DataRequired()], min_entries=1)
-    Note = TextAreaField('Note')
+    items = FieldList(FormField(InvoiceItemForm), min_entries=1)
+    note = TextAreaField('Note')
     submit = SubmitField('Generate Invoice')
--- Invoice/invoice/routes.py
+++ Invoice/invoice/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import render_template, Blueprint
+from flask_login import login_required
+from invoice.invoice.forms import InvoiceForm
 
 invoice = Blueprint('invoice', __name__)
 
@@ -8,5 +10,7 @@ def invoices():
     return '<h1>All invoice</h1>'
 
 @invoice.route('/add-new')
+@login_required
 def add_invoice():
-    return '<h1>Add New invoice</h1>'
+    form = InvoiceForm()
+    return render_template('invoice/addinvoice.html', title='Invoice', form=form)
