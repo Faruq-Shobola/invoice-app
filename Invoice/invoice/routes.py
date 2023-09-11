fbc3d4705a9980e01d26f20df53150dd61c82b91
shobola faruq <shobolafaruq@gmail.com>
2023-09-11 04:08:21 +0100

    Get data from form then add it to db

--- Invoice/invoice/routes.py
+++ Invoice/invoice/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint
-from flask_login import login_required
+from flask_login import login_required, current_user
 from invoice.invoice.forms import InvoiceForm
+from invoice.models import Client, Invoice, InvoiceItem
 
 invoice = Blueprint('invoice', __name__)
 
@@ -9,10 +10,12 @@ invoice = Blueprint('invoice', __name__)
 def invoices():
     return '<h1>All invoice</h1>'
 
-@invoice.route('/add-new')
+@invoice.route('/add-new', methods=['GET', 'POST'])
 @login_required
 def add_invoice():
     form = InvoiceForm()
+    form.client.choices = [(client.id, client.name) for client in Client.query.filter_by(customer=current_user).order_by(Client.name).all()]
+
     if form.validate_on_submit():
         client = form.client.data
         invoice_date = form.invoice_date.data
@@ -27,4 +30,13 @@ def add_invoice():
             rate = item.rate
             quantity = item.quantity
             
+            item = InvoiceItem(name=item_name, details=item_details, rate=rate, quantity=quantity)
+            
+        invoice = Invoice(invoice_id=1, invoice_date=invoice_date, due_date=due_date, note=note, labour=labour)
+        invoice.items.append(item)
+        
+        print(invoice)
+        
+        db.session.add(invoice)
+
     return render_template('invoice/addinvoice.html', title='Invoice', form=form)
